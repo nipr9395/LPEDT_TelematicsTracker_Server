4d3e89ceb949d0d587f9179b7188d236c94ecb67
Nik Provost <nipr9395@colorado.edu>
2023-05-08 00:54:40 -0600

    Update UDP_Server.py

--- UDP_Server.py
+++ UDP_Server.py
@@ -3,12 +3,13 @@ import socket
 import psycopg2
 
 # set up UDP socket
-UDP_PORT =5000#os.environ.get('PORT',5000)
-#DB_HOST = os.environ.get('DB_HOST')
-#DB_PORT = os.environ.get('DB_PORT')
-#DB_NAME = os.environ.get('DB_NAME')
-#DB_USER = os.environ.get('DB_USER')
-#DB_PASSWORD = os.environ.get('DB_PASSWORD')
+IP = "0.0.0.0"
+PORT =5005
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DATABASE_URL = os.environ['DATABASE_URL']
 
 
@@ -16,8 +17,8 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 #Bind to the UPD Socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("0.0.0.0", UDP_PORT))
+sock = socket.socket(socket.AF_INET, socket.socket.SOCK_DGRAM)
+sock.bind((IP, PORT))
 
 #Something to tell us its alive
 print("Hello world")
@@ -25,12 +26,12 @@ print("Hello world")
 # listen for incoming packets and insert data into database
 while True:
     data, addr = sock.recvfrom(1024)
-    query = data.decode().strip()
-    print(f"Received query '{query}' from {addr}")
+    ##query = data.decode().strip()
+    print(f"Received query '{data}' from {addr}")
 
-    with conn.cursor() as cur:
-        cur.execute(query)
-        results = cur.fetchall()
-        print(f"Query returned: {results}")
+    #with conn.cursor() as cur:
+        #cur.execute(query)
+        #results = cur.fetchall()
+        #print(f"Query returned: {results}")
 
-    conn.commit()
+    #conn.commit()
